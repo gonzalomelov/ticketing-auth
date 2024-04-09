be884ad740dc4171f10375507a8ff60902bbf4b5
Gonzalo Melo <gonzalomelov@gmail.com>
2024-04-09 19:56:01 -0300

    Add routes for each API method. Add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
+import { errorHandler } from './middlewares/error-handler';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-})
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on 3000');

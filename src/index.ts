a9f1885be9affb03e897a48b6115e88e30888792
Gonzalo Melo <gonzalomelov@gmail.com>
2024-04-11 19:00:44 -0300

    Add express-async-errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';

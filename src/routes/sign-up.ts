cb6a9eeddf80004ca53b1b13b8767a2035bdae06
Gonzalo Melo <gonzalomelov@gmail.com>
2024-04-25 16:10:24 -0300

    Remove unused deps

--- src/routes/sign-up.ts
+++ src/routes/sign-up.ts
@@ -2,7 +2,6 @@ import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user'
 

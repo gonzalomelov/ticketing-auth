7f868ee9af6a8768e127b8c2b5fc953aeae90125
Gonzalo Melo <gonzalomelov@gmail.com>
2024-05-04 19:49:47 -0300

    Add tests using Jest. Signup tests finished.

---
+++ src/routes/__test__/signup.test.ts
@@ -0,0 +1,83 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns 201 on a successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+})
+
+it('returns 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: 'password'
+    })
+    .expect(400);
+})
+
+it('returns 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p'
+    })
+    .expect(400);
+})
+
+it('returns 400 with missing email and/or password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password'
+    })
+    .expect(400);
+  
+  return request(app)
+    .post('/api/users/signup')
+    .send({})
+    .expect(400);
+})
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400);
+})
+
+it('responds with a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  expect(response.headers['set-cookie']).toBeDefined();
+});

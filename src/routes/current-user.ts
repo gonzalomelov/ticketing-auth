d447c53a8fc19e0f8e140713a8823ca4c53870ea
Gonzalo Melo <gonzalomelov@gmail.com>
2024-05-04 10:24:12 -0300

    Implement current-user endpoint

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,36 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: string | JwtPayload | null;
+    }
+  }
+}
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+const tryAuthenticate = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.jwt) {
+    req.user = null;
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    req.user = payload;
+  } catch (err) {
+    req.user = null;
+  }
+
+  next();
+}
+
+const currentUser = (req: Request, res: Response) => {
+  return res.send({ currentUser: req.user });
+};
+
+router.get('/api/users/currentuser', tryAuthenticate, currentUser);
 
 export { router as currentUserRouter };

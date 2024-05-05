16770e5302a933780cf25b34b5ac45d1ab0c8d05
Gonzalo Melo <gonzalomelov@gmail.com>
2024-05-05 11:38:48 -0300

    Add current-user tests and create a reusable method to signup users

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  var signup: () => Promise<string>;
+}
 
 let mongo: MongoMemoryServer;
 
@@ -27,3 +33,20 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 })
+
+global.signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+  
+  const cookie = response.header['set-cookie'];
+
+  return cookie;
+}
